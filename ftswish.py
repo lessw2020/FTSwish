8d31254c97079b3950d7efc5882e3104160a1d38
Less Wright <lessw@etrillium.com>
2019-04-19 23:28:10 -0700

    Update ftswish.py

--- ftswish.py
+++ ftswish.py
@@ -6,10 +6,14 @@ class FTSwish(nn.Module):
         self.threshold,self.sub,self.maxv = threshold,sub,maxv
 
     def forward(self, x): 
-        if x > 0:
-            x = (x*torch.sigmoid(x)) + self.threshold
-        else:
-            x = threshold
+        
+        pos_value = (x*torch.sigmoid(x)) + self.threshold
+        #this is temp workaround - CPU vs CUDA conflict
+        cuda0 = torch.device('cuda:0')
+        
+        tval = torch.tensor([self.threshold],device=cuda0)
+        
+        x = torch.where(x>=0,pos_value, tval)
 
         if self.sub is not None: x.sub_(self.sub)
         if self.maxv is not None: x.clamp_max_(self.maxv)
